b6531e0e2836693261a7fa509db695e9f2eafb0f
Abhii30 <akumar300599@gmail.com>
2023-02-08 16:13:57 +0530

    update login v1.1

--- src/Screen/LoginPage.js
+++ src/Screen/LoginPage.js
@@ -25,12 +25,11 @@ const passwordReducer = (state, actions) => {
 };
 
 const LoginPage = () => {
-
   const setIsLogin = userStore((state) => state.setIsLogin);
 
   const { manasInstance } = useContext(AuthContext);
 
-  const [ adminValue, setAdminValue ] = useState(false);
+  const [adminValue, setAdminValue] = useState(false);
 
   const [emailState, dispatchEmail] = useReducer(emailReducer, {
     value: "",
@@ -60,23 +59,50 @@ const LoginPage = () => {
 
   const adminChangeHandler = () => {
     console.log(adminValue);
-    setAdminValue((value) => !value)
+    setAdminValue((value) => !value);
   };
 
   const submitHandler = async (event) => {
     event.preventDefault();
-    const user = adminValue ? 'admin' : 'student';
-    const res = await manasInstance.loginHandler(emailState.value, passwordState.value, user);
-    if(res.status === 201) {
+    const user = adminValue ? "admin" : "student";
+    const res = await manasInstance.loginHandler(
+      emailState.value,
+      passwordState.value,
+      user
+    );
+    if (res.status === 201) {
       setIsLogin(true, false);
     }
   };
 
+  const switchStudent = () => {
+    // document.getElementById("admin").style.display = "none";
+    document.getElementById("student").style.left = "0";
+    document.getElementById("btn").style.left = "50%";
+    document.getElementById("admin").style.left = "-121%";
+  };
+  const switchAdmin = () => {
+    document.getElementById("admin").style.left = "0";
+    document.getElementById("btn").style.left = "0";
+    document.getElementById("student").style.left = "121%";
+  };
+
   return (
-      <div className={styles.loginMain}>
-        <div className={styles.loginContainer}>
-          <h2 className={styles.headingLogin}>Log In</h2>
-          <form className={styles.loginForm} onSubmit={submitHandler}>
+    <div className={styles.loginMain}>
+      <div className={styles.loginContainer}>
+        <div className={styles.toggle}>
+          <div id="btn" className={styles.btn}></div>
+          <button onClick={switchAdmin}>Admin</button>
+          {/* <hr></hr> */}
+          <button onClick={switchStudent}>Student</button>
+        </div>
+        <h2 className={styles.headingLogin}>Log In</h2>
+        <div className={styles.formContainer}>
+          <form
+            id="student"
+            className={styles.loginFormStudent}
+            onSubmit={submitHandler}
+          >
             <Input
               id="email"
               type="email"
@@ -95,17 +121,51 @@ const LoginPage = () => {
               value={passwordState.value}
               isValid={passwordState.isValid}
             />
-            <Input className={styles.radio}
+            <Input
+              className={styles.radio}
               id="radio"
               type="checkbox"
               label="Admin"
               onChange={adminChangeHandler}
             />
-              <button className={styles.loginButton}>Submit</button>
+            <button className={styles.loginButton}>Submit</button>
+          </form>
+          <form
+            id="admin"
+            className={styles.loginFormAdmin}
+            onSubmit={submitHandler}
+          >
+            <Input
+              id="email"
+              type="email"
+              label="E-Mail Admin"
+              onChange={emailChangeHandler}
+              onBlur={validateEmailHandler}
+              value={emailState.value}
+              isValid={emailState.isValid}
+            />
+            <Input
+              id="password"
+              type="password"
+              label="Password"
+              onChange={passwordChangeHandler}
+              onBlur={validatePasswordHandler}
+              value={passwordState.value}
+              isValid={passwordState.isValid}
+            />
+            <Input
+              className={styles.radio}
+              id="radio"
+              type="checkbox"
+              label="Admin"
+              onChange={adminChangeHandler}
+            />
+            <button className={styles.loginButton}>Submit</button>
           </form>
         </div>
       </div>
+    </div>
   );
-}
+};
 
 export default LoginPage;
