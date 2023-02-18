86c689a7d3d2c644803aee8906992b2b10c3b1f4
Abhii30 <akumar300599@gmail.com>
2023-02-18 18:20:35 +0530

    added teams

--- src/Components/OurTeam.js
+++ src/Components/OurTeam.js
@@ -3,27 +3,30 @@ import styles from "./OurTeam.module.css";
 
 const OurTeam = () => {
   return (
-    <div>
-      <div>
+    <div className={styles.ourTeam}>
+      <div className={styles.teamAbout}>
         <h2 className={styles.heading}>Our Team</h2>
-        <p className={styles.subtitle}>"Change does not roll on the wheels of inevitability, but comes through continuous struggle."</p>
+        <p className={styles.subtitle}>
+          "Change does not roll on the wheels of inevitability, but comes
+          through continuous struggle."
+        </p>
       </div>
       <div className={styles.team}>
-        {teamData.map(team => {
+        {teamData.map((team) => {
           return (
             <div className={styles.member}>
-              <div>
-                {/* <img src={team.image} alt="member" /> */}
+              <div className={styles.imageTeam}>
+                <img src={team.image} alt="member" />
               </div>
               <h2>{team.name}</h2>
               <h3>{team.designation}</h3>
               <p>{team.text}</p>
             </div>
-          )
+          );
         })}
       </div>
     </div>
-  )
+  );
 };
 
 export default OurTeam;
--- src/lib/data.js
+++ src/lib/data.js
@@ -1,71 +1,69 @@
-import directorDP from '../resource/profilePic/directorDP.jpeg';
-import profile1 from '../resource/profilePic/profile1.png';
-import profile2 from '../resource/profilePic/profile2.png';
-import profile3 from '../resource/profilePic/profile3.png';
-import profile4 from '../resource/profilePic/profile4.png';
-import teacher1 from '../resource/profilePic/teacher1.jpeg';
-import teacher2 from '../resource/profilePic/teacher2.jpeg';
+import directorDP from "../resource/profilePic/directorDP.jpeg";
+import profile1 from "../resource/profilePic/profile1.png";
+import profile2 from "../resource/profilePic/profile2.png";
+import profile3 from "../resource/profilePic/profile3.png";
+import profile4 from "../resource/profilePic/profile4.png";
+import teacher1 from "../resource/profilePic/teacher1.jpeg";
+import teacher2 from "../resource/profilePic/teacher2.jpeg";
 
 export const reviews = [
   {
     id: 1,
-    name: 'Aman Kumar',
-    job: 'IIT Bombay',
+    name: "Aman Kumar",
+    job: "IIT Bombay",
     image: profile1,
-    text:
-      "Myself Aman Kumar, an alumni of Manas Education Centre. I completed my preparation for the JEE examination from here only and successfully cracked the same in 2019, acquiring AIR 1980 and OBC rank 273. I am currently pursuing my graduation from one of the best Institute of India, INDIAN INSTITUTE OF TECHNOLOGY, BOMBAY under mechanical department. And all these things were only possible because of our Manas Education Centre and it's faculties, who made me able to achieve these.",
+    text: "Myself Aman Kumar, an alumni of Manas Education Centre. I completed my preparation for the JEE examination from here only and successfully cracked the same in 2019, acquiring AIR 1980 and OBC rank 273. I am currently pursuing my graduation from one of the best Institute of India, INDIAN INSTITUTE OF TECHNOLOGY, BOMBAY under mechanical department. And all these things were only possible because of our Manas Education Centre and it's faculties, who made me able to achieve these.",
   },
   {
     id: 2,
-    name: 'Ankit Tarway',
-    job: 'IIT(ISM), Dhanbad',
+    name: "Ankit Tarway",
+    job: "IIT(ISM), Dhanbad",
     image: profile2,
-    text:
-      'One of the best descion I made in my life was studying in Manas, Teachers here are more than just teachers, they are here to help you in every phase of life from preparation to post college. It has been more than 8 years, i still hang around with same friends i made here and still come to same teachers to resolve my doubts. It just they are not sheets and DPP any more.',
+    text: "One of the best descion I made in my life was studying in Manas, Teachers here are more than just teachers, they are here to help you in every phase of life from preparation to post college. It has been more than 8 years, i still hang around with same friends i made here and still come to same teachers to resolve my doubts. It just they are not sheets and DPP any more.",
   },
   {
     id: 3,
-    name: 'Tarun Kumar',
-    job: 'Jadavpur University',
+    name: "Tarun Kumar",
+    job: "Jadavpur University",
     image: profile3,
-    text:
-      'I have been brought up in Gaya.I wanted to go to Kota for JEE preparation but due to financial issues, I joined MANAS which provided a competing environment needed for preparation. DPPs , practice sheets , regular doubt clearance and technical alongwith emotional support of faculties helped me land in a university with almost zero fees for bachelors. I was able to continue my engineering easily and right now doing a govt job. I really want to thank Manas Education Centre specially pushkar sir for making me such a successful person.',
+    text: "I have been brought up in Gaya.I wanted to go to Kota for JEE preparation but due to financial issues, I joined MANAS which provided a competing environment needed for preparation. DPPs , practice sheets , regular doubt clearance and technical alongwith emotional support of faculties helped me land in a university with almost zero fees for bachelors. I was able to continue my engineering easily and right now doing a govt job. I really want to thank Manas Education Centre specially pushkar sir for making me such a successful person.",
   },
   {
     id: 4,
-    name: 'Satyam Kumar',
-    job: 'NIT Raipur',
+    name: "Satyam Kumar",
+    job: "NIT Raipur",
     image: profile4,
-    text:
-      'Firstly, I would like to thanks to all teachers of MANAS Team for giving me this opportunity. I would like to share that what motivated me throughout my journey in MANAS during JEE preparation. I joined MANAS in 2016. And I can clearly remember that My name used to come in last rows of the result list of Weekly Tests in 11th . But In Manas, there was a ritual to give Pen as a Reward to First 3 Toppers of every Test infront of the class. And that became the motivation for me that someday I would be receiving that pen infront of the whole class and I started following Pushkar Sir and Dharmendra Sir. I tried my best to be consistent and revise whatever they used to teach on daily basis. Their lectures were very helpful in getting more focused and being disciplined. I am very lucky that they were my teachers. With the help of their guidance, I got the opportunity to win those pens every week in Tests in 12th and also Secured AIR - 8090 ( General ) in JEE - Advanced 2018.Currently, I am working In ADANI Groups. Thanks MANAS and the Team.',
+    text: "Firstly, I would like to thanks to all teachers of MANAS Team for giving me this opportunity. I would like to share that what motivated me throughout my journey in MANAS during JEE preparation. I joined MANAS in 2016. And I can clearly remember that My name used to come in last rows of the result list of Weekly Tests in 11th . But In Manas, there was a ritual to give Pen as a Reward to First 3 Toppers of every Test infront of the class. And that became the motivation for me that someday I would be receiving that pen infront of the whole class and I started following Pushkar Sir and Dharmendra Sir. I tried my best to be consistent and revise whatever they used to teach on daily basis. Their lectures were very helpful in getting more focused and being disciplined. I am very lucky that they were my teachers. With the help of their guidance, I got the opportunity to win those pens every week in Tests in 12th and also Secured AIR - 8090 ( General ) in JEE - Advanced 2018.Currently, I am working In ADANI Groups. Thanks MANAS and the Team.",
   },
 ];
 
 export const directorProfile = {
-  name: 'Pushkar Srigyan',
-  job: '(B.Tech, IIT Delhi)',
+  name: "Pushkar Srigyan",
+  job: "(B.Tech, IIT Delhi)",
   image: directorDP,
-  text:
-    "For last 8 years of his passion for guiding students to achieve them in various Competitive examinations has given a new direction in education sector. He completed his schooling from Netarhat Residential School(Jharkhand) and received his B.Tech degree from IIT Delhi. He is a student favorite when it comes to imbiling and imparting knowledge to students. He is a renowned speaker And a motivation Guru. He has deep understanding of the students psyche and keeps bring new perspective to solution designing process. His profound insights and examples make the student grasp the subject practically. Going one step further he makes the class interesting relevant for the students. He has the ability to even make the weakest student understand the subject.",
+  text: "For last 8 years of his passion for guiding students to achieve them in various Competitive examinations has given a new direction in education sector. He completed his schooling from Netarhat Residential School(Jharkhand) and received his B.Tech degree from IIT Delhi. He is a student favorite when it comes to imbiling and imparting knowledge to students. He is a renowned speaker And a motivation Guru. He has deep understanding of the students psyche and keeps bring new perspective to solution designing process. His profound insights and examples make the student grasp the subject practically. Going one step further he makes the class interesting relevant for the students. He has the ability to even make the weakest student understand the subject.",
 };
 
 export const teamData = [
   {
-    name: 'Praneet Kumar',
-    designation: 'Ex. Tilaiyan',
+    name: "Praneet Kumar",
+    designation: "Ex. Tilaiyan",
     image: teacher1,
-    text: 'Alumni of TS Chanakya Mumbai',
-  },
-  {
-    name: 'Prabhakar Kumar',
-    designation: 'Ex. Netarhatian',
-    image: teacher2,
-    text: 'Alumni of TS Chanakya Mumbai',
-  },
-  {
-    name: 'Pushkar Srigyan',
-    designation: 'Ex. Netarhatian',
+    text: "Alumni of TS Chanakya Mumbai",
     image: directorDP,
-    text: 'Humanistic Education Enthusiast',
-  }
+  },
+  {
+    name: "Prabhakar Kumar",
+    designation: "Ex. Netarhatian",
+    image: teacher2,
+    text: "Alumni of TS Chanakya Mumbai",
+    image: directorDP,
+  },
+  {
+    name: "Pushkar Srigyan",
+    designation: "Ex. Netarhatian",
+    image: directorDP,
+    text: "Humanistic Education Enthusiast",
+    image: directorDP,
+  },
 ];
