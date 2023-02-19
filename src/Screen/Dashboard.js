5a89286a851bea9c54f7c6504b6be246935d4d70
Fahad Ali <alifahad2210@gmail.com>
2023-02-19 23:33:46 +0530

    popup

--- src/Screen/Dashboard.js
+++ src/Screen/Dashboard.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import Recent from "../Components/Recent";
-import TagLine from "../Components/TagLine";
 import ReviewsSlider from "../Components/ReviewsSlider";
 import Contact from "../Components/Contact";
 import OurTeam from "../Components/OurTeam";
+import Modal from "../UI/Modal";
+import image from '../resource/displayPic/popup.jpeg'
 
 function Dashboard() {
+
+  const [showModal, setShowModal] = useState(true);
+
+  const closeModalHandler = () => {
+    setShowModal(false);
+  }
+
   return (
     <div>
+      {showModal && (
+        <Modal onClose={closeModalHandler}>
+          <div>
+            <img src={image} alt="manas" />
+          </div>
+        </Modal>
+      )}
       <Recent />
       {/* <TagLine /> */}
       <OurTeam />
