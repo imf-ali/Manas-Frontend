1ee6f598a9e432504391290ca3ba382790060e5b
Abhii30 <akumar300599@gmail.com>
2023-02-21 15:26:02 +0530

    update tagLine

--- src/Components/TagLine.js
+++ src/Components/TagLine.js
@@ -5,14 +5,18 @@ import image from '../resource/displayPic/manasHand.jpeg';
 function TagLine() {
   return (
     <div className={styles.contestsMain}>
-      <div className={styles.imagediv}>
-        <img className={styles.image} src={image} alt="manas" />
-      </div>
       <div className={styles.contentdiv}>
         <h2 className={styles.heading}>Manas Education Centre</h2>
         <div className={styles.contestNames}>
-          "We provide classroom program for IIT JEE preparation in pattern proof mode. Our team of IITian faculties are dedicated and devoted of highest caliber with a genuine concern for building student's future. Our scientific approach has produced highest Success ratio in JEE Main & JEE Advanced across all over India."
+          "We provide classroom program for IIT JEE preparation in pattern proof
+          mode. Our team of IITian faculties are dedicated and devoted of
+          highest caliber with a genuine concern for building student's future.
+          Our scientific approach has produced highest Success ratio in JEE Main
+          & JEE Advanced across all over India."
         </div>
+      </div>
+      <div className={styles.imagediv}>
+        <img className={styles.image} src={image} alt="manas" />
       </div>
     </div>
   );
