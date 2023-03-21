19b594c4df433f5486a92e0ee5b6f421f3b7259f
Fahad Ali <alifahad2210@gmail.com>
2023-03-21 19:02:24 +0530

    array changes

--- src/Components/Teaching.js
+++ src/Components/Teaching.js
@@ -9,19 +9,18 @@ const Teaching = () => {
       </div>
       <Card 
         title="Our Macro & Micro Analysis for Students"
-        subtitle="
-        Test Pattern : Subjective pattern, objective pattern Type of Tests : Class Test, Weekly Test, Unit Test, Middle session test, Test Series Full Test, Scholarship Test"
+        subtitle={["Test Pattern : Subjective pattern", "objective pattern Type of Tests : Class Test, Weekly Test, Unit Test, Middle session test, Test Series Full Test, Scholarship Test"]}
       />
       <Card 
         title="Self Study Program Under Guidance"
-        subtitle="- 2 to 4 Hrs Every Day
-        - Practice Session
-        - Doubt Clearing Session"
+        subtitle={["2 to 4 Hrs Every Day",
+        "Practice Session", 
+        "Doubt Clearing Session"]}
       />
       <Card 
         title="Our scholarship ProgramM"
-        subtitle="- Admission CUM Scholarship Program
-        - Aptitude Test CUM Scholarship Program"
+        subtitle={["Admission CUM Scholarship Program", 
+        "Aptitude Test CUM Scholarship Program"]}
       />
     </>
   )
